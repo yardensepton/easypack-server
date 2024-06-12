3c86a5696c10bb40d95106aa2062b65b48b83919
yardensepton <yardensepton@gmail.com>
2024-06-12 20:26:24 +0300

    made templates responsive

--- src/routers/user_route.py
+++ src/routers/user_route.py
@@ -55,15 +55,16 @@ async def login_user(user_data: OAuth2PasswordRequestForm = Depends()):
 async def user_forgot_password(request: Request, user_email: EmailStr):
     user: UserEntity = user_controller.get_user_by_email(user_email)
     if user:
-        # access_token = create_access_token(user_id=user.id)
         # TODO: add email to parmeter
         url = f"{request.base_url}users/reset-password-template?user_id={user.id}"
         short_url = shortener.tinyurl.short(url)
         await send_reset_password_mail(recipient_email=user_email, user=user, url=short_url,
                                        expire_in_minutes=60)
-    return {
-        "result": f"An email has been sent to {user_email} with a link for password reset."
-    }
+        return {
+            "result": f"An email has been sent to {user_email} with a link for password reset."
+        }
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 
 @router.get("/reset-password-template")
